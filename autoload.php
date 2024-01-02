813163b3cd02bfbf07d416fc166b3d0cde602b44
AlinaLedova <alinaledova@friday13.team>
2024-01-03 00:25:28 +0300

    Added autoload file

---
+++ autoload.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Algorythms\DataStructures
+{
+    function LoadDataStructure(string $fullClassName): bool
+    {
+        $parts = explode('\\', $fullClassName);
+        array_shift($parts);
+        $expectedFilePath = __DIR__ . '/' . implode('/', $parts) . '.php';
+
+        if (!file_exists($expectedFilePath))
+            return false;
+
+        require_once $expectedFilePath;
+
+        if (!class_exists($fullClassName))
+            return false;
+
+        return true;
+    }
+
+    spl_autoload_register(__NAMESPACE__ . '\LoadDataStructure');
+}
